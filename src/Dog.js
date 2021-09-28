8dcb4eca25178baa5eb1106c3c8576cc136fe221
sonia1183 <80530004+sonia1183@users.noreply.github.com>
2021-09-28 10:36:01 +0530

    22

--- src/Dog.js
+++ src/Dog.js
@@ -1,12 +1,8 @@
-
-
-const Dog = () =>{
-    return React.createElement(
-        "div",{},[
-            React.createElement('h3',{},"Bull Terrier"),
-            React.createElement('h3',{},"50.70"),
-            React.createElement('h3',{},"Terrier"),
-        ]
-    )
-}
+const Dog = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h3", {}, "Bull Terrier"),
+    React.createElement("h3", {}, "50.70"),
+    React.createElement("h3", {}, "Terrier"),
+  ]);
+};
 export default Dog;
