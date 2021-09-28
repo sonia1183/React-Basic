f427fd2b7af96f959529c4921e6b4f2771e34c8a
sonia1183 <80530004+sonia1183@users.noreply.github.com>
2021-09-28 10:50:38 +0530

    333

--- src/Dog.js
+++ src/Dog.js
@@ -1,8 +1,10 @@
-const Dog = () => {
+import React from 'react';
+const Dog = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h3", {}, "Bull Terrier"),
-    React.createElement("h3", {}, "50.70"),
-    React.createElement("h3", {}, "Terrier"),
+    React.createElement("h3", {}, props.name),
+    React.createElement("h3", {}, props.weight),
+    React.createElement("h3", {}, props.breed),
+    //React.createElement('h3',{},props.height), //not find, no error
   ]);
 };
 export default Dog;
